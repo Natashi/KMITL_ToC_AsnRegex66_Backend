04b7e13377e7234e8ccca1e8cf832ec156abc028
Natashi <32347635+Natashi@users.noreply.github.com>
2023-03-28 19:50:38 +0700

    Add project files

---
+++ app.py
@@ -0,0 +1,17 @@
+import scraping_ver_06
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.get("/all_temples")
+def get_temples():
+    return jsonify(scraping_ver_06.get_temples_data())
+
+@app.get("/province_temple/<string:province_name>")
+def get_temple_in_province(province_name):
+	try:
+		province_name = province_name.strip().lower()
+		res = scraping_ver_06.get_temples_data(province_name)
+		return jsonify([x for key in res for x in res[key]])
+	except ValueError:
+		return "Province not supported", 400
